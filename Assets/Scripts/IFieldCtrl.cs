cd687b430e5a1aa718fcaa6c5feef88a043b7cff
KinderSheep6938 <hakodate.sirokuro4696@gmail.com>
2023-10-17 22:09:57 +0900

    ミノシステム関連続き + フィールドシステム作成開始

---
+++ Assets/Scripts/IFieldCtrl.cs
@@ -0,0 +1,15 @@
+// -------------------------------------------------------------------------------
+// IFieldCtrl.Interface
+//
+// çÏê¨ì˙: 2023/10/17
+// çÏê¨é“: Shizuku
+// -------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFieldCtrl
+{
+    bool CheckAlreadyMinoExist(int x, int y);
+    void SetMino(int x,int y);
+}
---
+++ Assets/Scripts/ILineMinoCtrl.cs
@@ -0,0 +1,15 @@
+// -------------------------------------------------------------------------------
+// ILineMinoCtrl.Interface
+//
+// çÏê¨ì˙: 2023/10/17
+// çÏê¨é“: Shizuku
+// -------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILineMinoCtrl
+{
+    void DownMino();
+    void DeleteMino();
+}
