b134fbb98beabbc0ea5720762d28706ec4b964dd
STD\s20227039 <hakodate.sirokuro4696@gmail.com>
2023-10-17 15:02:17 +0900

    フォルダ作成＋IMinoCtrl.cs作成開始

---
+++ Assets/Scripts/IMinoCtrl.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IMinoCtrl
+{
+    int MinoX { get; set; }
+    int MinoY { get; set; }
+    Color MinoColor { get; set; }
+}
